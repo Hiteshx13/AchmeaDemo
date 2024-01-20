9e4c75e283c2079c4106a3afe072702ecc86be2c
Hitesh Modhwadia <imhr1727@gmail.com>
2024-01-20 05:43:13 +0200

    initial commit

---
+++ app/src/main/java/com/achmea/demo/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.achmea.demo.common
+
+object Constants {
+    const val BASE_URL =
+        "https://cba.kooijmans.nl"
+    ///CBAEmployerservice.svc/rest/employers?filter=Achmea&maxRows=100
+}
