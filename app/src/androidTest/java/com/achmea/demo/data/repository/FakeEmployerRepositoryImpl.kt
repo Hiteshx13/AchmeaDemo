57befb1530fef96bab7125b3914707a2ffc29b28
Hitesh Modhwadia <imhr1727@gmail.com>
2024-01-22 04:39:32 +0200

    Unit test return type changes inside FakeEmployerRepositoryImpl

--- app/src/androidTest/java/com/achmea/demo/data/repository/FakeEmployerRepositoryImpl.kt
+++ app/src/androidTest/java/com/achmea/demo/data/repository/FakeEmployerRepositoryImpl.kt
@@ -1,12 +1,13 @@
 package com.achmea.demo.data.repository
 
+import com.achmea.demo.common.DataState
 import com.achmea.demo.domain.model.Employer
 import com.achmea.demo.domain.repository.EmployerRepository
 
 class FakeEmployerRepositoryImpl : EmployerRepository {
     val list= mutableListOf<Employer>()
-    override suspend fun getEmployerData(filter: String, maxRows: Int?): List<Employer> {
-        return list
+    override suspend fun getEmployerData(filter: String, maxRows: Int?): DataState<List<Employer>> {
+        return DataState.Success(list)
     }
 
     override suspend fun getAllCachedEmployers(): List<Employer> {
